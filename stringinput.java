769c0fd7c961ff44a3fe55fbdbfce503466f9171
Jayasri-Shanmugam <138643993+Jayasri-Shanmugam@users.noreply.github.com>
2023-07-15 18:53:51 +0530

    Add files via upload

---
+++ stringinput.java
@@ -0,0 +1,24 @@
+package Jayasrijavapractice;
+
+import java.util.Scanner;
+
+public class stringinput {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		Scanner input=new Scanner(System.in);
+	    String word=input.nextLine();
+	    System.out.println(word);
+	    
+	    int a=10;
+	    input.nextLine();
+	    String word2=input.nextLine();
+	    System.out.println(word2);
+		
+		
+
+	}
+
+}
